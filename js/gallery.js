22085d3c5aae9a98b811f87a7bf06312a1fa7d50
Aleksandar Nanushevski <alexandarpedro@gmail.com>
2020-05-02 20:23:03 +0200

    proba za crtanje:Neuspeshna

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,53 @@
-function drawGallery(name , imageSrc ,imageAlt){
-    divImage= document.createElement("div");
-    divImage.className="container";
+function drawGallery(name, imageSrc, imageAlt) {
+    divImage = document.createElement("div");
+    divImage.className = "container";
 
-    var imageElement=document.createElement("img");
-    imageElement.src=imageSrc;
-    imageElement.alt=imageAlt;
-    imageElement.className="image"
+    var imageElement = document.createElement("img");
+    imageElement.src = imageSrc;
+    imageElement.alt = imageAlt;
+    imageElement.className = "image"
 
-    var middleDiv=document.createElement("div");
-    middleDiv.className=("middle")
-    
-    var textDiv=document.createElement("div");
-    textDiv.className=("text");
-    textDiv.innerText=name;
+    var middleDiv = document.createElement("div");
+    middleDiv.className = ("middle")
 
-    divImage.append(imageElement,middleDiv,textDiv);
+    var textDiv = document.createElement("div");
+    textDiv.className = ("text");
+    textDiv.innerText = name;
+
+    divImage.append(imageElement, middleDiv, textDiv);
     return divImage
 
+}
+
+var allImages;
+function getAllImages() {
+    fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master/data/gallery.json")
+        .then(function (res) {
+            return res.json();
+        }).then(function (data) {
+            
+
+            allImages = Object.keys(data).map(function (key) {
+                var newElement = {
+                    image: data[key].image,
+                    name: data[key].name
+                };
+                return newElement;
+
+            });
+            return drewElementsInGall(allImages);
+        }).catch(function (error){
+            console.log(error);
+        });
+
+}
+
+function drewElementsInGall(arrayOfImages) {
+    arrayOfImages.forEach(function (image) {
+        var galImage = drawGallery(
+            image.image,
+            image.name
+        );
+        document.getElementById("gallery").append(galImage)
+    });
 }
