ed5a13f69b1a632ace232fbd178af58a4d1ff842
Aleksandar Nanushevski <alexandarpedro@gmail.com>
2020-05-06 22:26:14 +0200

    changes

--- js/gallery.js
+++ js/gallery.js
@@ -1,27 +1,34 @@
 fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master/data/gallery.json")
-.then(function(response){
-    return response.json()
-}).then(function (data){
-    drawImages(data)
-})
-function drawImages(arrayOfImages){
- for (let i = 0; i < arrayOfImages.length; i++) {
-     var div= document.createElement("div");
-     div.setAttribute("class", "container")
-     var imageProperty=document.createElement("img");
-     imageProperty.setAttribute("src",arrayOfImages[i].image);
-     imageProperty.setAttribute("class","image")
-     imageProperty.style.width="100%";
-     middleDiv=document.createElement("div");
-     middleDiv.setAttribute("class","middle");
-     textDiv= document.createElement("div");
-     textDiv.innerText=arrayOfImages[i].name;
-     textDiv.setAttribute("class", "text")
-     
-    middleDiv.append(textDiv);     
-     div.append(imageProperty,middleDiv);
-     document.body.appendChild(div)
- }
-    
+    .then(function (response) {
+        return response.json()
+    }).then(function (data) {
+        drawImages(data)
+    })
+function drawImages(arrayOfImages) {
+    for (let i = 0; i < arrayOfImages.length; i++) {
+        var div = document.createElement("div");
+        div.setAttribute("class", "container");
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+        var divMd = document.createElement("div");
+        divMd.setAttribute("class", "col-lg-5")
+        var imageProperty = document.createElement("img");
+        imageProperty.setAttribute("src", arrayOfImages[i].image);
+        imageProperty.setAttribute("class", "image")
+        imageProperty.style.width = "100%";
+        middleDiv = document.createElement("div");
+        middleDiv.setAttribute("class", "middle");
+        textDiv = document.createElement("div");
+        textDiv.innerText = arrayOfImages[i].name;
+        textDiv.setAttribute("class", "text")
+
+
+        middleDiv.append(textDiv);
+        divMd.append(imageProperty, middleDiv);
+        row.append(divMd);
+        div.append(row)
+        document.body.appendChild(div)
+    }
+
 }
 drawImages()
