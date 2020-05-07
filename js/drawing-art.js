f127ce084fc0995cc9ca191d9dfaa4dc2e6ca686
Aleksandar Nanushevski <alexandarpedro@gmail.com>
2020-05-07 10:14:21 +0200

    changes

--- js/drawing-art.js
+++ js/drawing-art.js
@@ -2,25 +2,28 @@ fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master
     .then(function (res) {
         return res.json()
     }).then(function (data) {
-        console.log(data)
+        console.log(data);
+        drawGallery(data);
 
     })
 
-function drewGallery(array) {
-    var div =createElement("div");
-    div.setAttribute("class","container")
-     for (let i = 0; i < array.length; i++) {
-        
+function drawGallery(arrayOfArt) {
+
+    for (let i = 0; i < arrayOfArt.length; i++) {
+        var div = document.createElement("div");
+        div.setAttribute("class", "container")
         var header1 = document.createElement("h3")
         header1.setAttribute("class", "headerTitle");
-        header1.innerText=array[i].title;
-        var disPara= document.createElement("p");
-        disPara.setAttribute("class","discription")
-        disPara.innerText=array[i].discription;
+        header1.innerText = arrayOfArt[i].title;
+        var disPara = document.createElement("p");
+        disPara.setAttribute("class", "discription")
+        disPara.innerText = arrayOfArt[i].discription;
 
-     
-        div.append(header1,disPara);
+
+        div.append(header1, disPara);
+        document.body.append(div)
     }
-    
-    document.body.append(div)
+
+
 }
+drewGallery();
