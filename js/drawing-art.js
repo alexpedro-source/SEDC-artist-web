ab0e52ec64e11d929bcef892d7d8fc8000f25285
Aleksandar Nanushevski <alexandarpedro@gmail.com>
2020-05-07 18:49:34 +0200

    changes

--- js/drawing-art.js
+++ js/drawing-art.js
@@ -2,33 +2,30 @@ fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master
     .then(function (res) {
         return res.json()
     }).then(function (data) {
-       console.log( Object.values(data));
-        drawGallery(data);
-        var imagesWithSameName=data.filter(function(image){
-            return image.name===""
-        })
+       console.log(data);
+      
 
 
     })
     
 
-function drawGallery(arrayOfArt) {
+// function drawGallery(arrayOfArt) {
     
-     for (let i = 0; i < arrayOfArt.length; i++) {
-        var div = document.createElement("div");
-        div.setAttribute("class", "container")
-        var header1 = document.createElement("h3")
-        header1.setAttribute("class", "headerTitle");
-        header1.innerText = arrayOfArt[i].title;
-        var disPara = document.createElement("p");
-        disPara.setAttribute("class", "discription")
-        disPara.innerText = arrayOfArt[i].discription;
+//      for (let i = 0; i < arrayOfArt.length; i++) {
+//         var div = document.createElement("div");
+//         div.setAttribute("class", "container")
+//         var header1 = document.createElement("h3")
+//         header1.setAttribute("class", "headerTitle");
+//         header1.innerText = arrayOfArt[i].title;
+//         var disPara = document.createElement("p");
+//         disPara.setAttribute("class", "discription")
+//         disPara.innerText = arrayOfArt[i].discription;
 
 
-        div.append(header1, disPara);
-        document.body.append(div)
-    }
+//         div.append(header1, disPara);
+//         document.body.append(div)
+//     }
 
 
-}
-drewGallery();
+// }
+// drewGallery();
