9641d9265c0024cf8d499e079fb851038216e520
Aleksandar Nanushevski <alexandarpedro@gmail.com>
2020-05-08 10:42:41 +0200

    changes

--- js/drawing-art.js
+++ js/drawing-art.js
@@ -1,31 +1,32 @@
+const urlParameters = new URLSearchParams(location.search)
+const name = urlParameters.get("name")
+
 fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master/data/images.json")
     .then(function (res) {
         return res.json()
     }).then(function (data) {
-       console.log(data);
-      
-
-
+        const onlyNeededGallery=data.filter(function(gallery){
+            return gallery.name==name;
+            
+        })
+       drawImages(onlyNeededGallery);
     })
+     function drawImages(image){
+        for (let i = 0; i <image.length; i++) {
+            var createdDiv= document.createElement("div");
+            createdDiv.setAttribute("class","container")
+            var imageProperty =document.createElement("img");
+            imageProperty.setAttribute("class","resize")
+            imageProperty.setAttribute("src",image[i].image)
+
+            createdDiv.append(imageProperty);
+            document.body.append(createdDiv)
+        }
+     }
+
     
-
-// function drawGallery(arrayOfArt) {
+        
+            
     
-//      for (let i = 0; i < arrayOfArt.length; i++) {
-//         var div = document.createElement("div");
-//         div.setAttribute("class", "container")
-//         var header1 = document.createElement("h3")
-//         header1.setAttribute("class", "headerTitle");
-//         header1.innerText = arrayOfArt[i].title;
-//         var disPara = document.createElement("p");
-//         disPara.setAttribute("class", "discription")
-//         disPara.innerText = arrayOfArt[i].discription;
-
-
-//         div.append(header1, disPara);
-//         document.body.append(div)
-//     }
-
-
-// }
-// drewGallery();
+        
+     
--- js/gallery.js
+++ js/gallery.js
@@ -2,18 +2,22 @@ fetch("https://raw.githubusercontent.com/alexpedro-source/SEDC-artist-web/master
     .then(function (response) {
         return response.json()
     }).then(function (data) {
-        console.log(data);
-        drawImages(data)
+
+        (drawImages(data));
     })
 function drawImages(arrayOfImages) {
+
     for (let i = 0; i < arrayOfImages.length; i++) {
+
         var div = document.createElement("div");
         div.setAttribute("class", "container");
-        
-    
+        div.onclick = function () {
+            location.assign("single-gallery.html?name=" + arrayOfImages[i].name)
+        }
+
         var imageProperty = document.createElement("img");
         imageProperty.setAttribute("src", arrayOfImages[i].image);
-        imageProperty.setAttribute("class", "image")
+        imageProperty.setAttribute("class", "img")
 
         middleDiv = document.createElement("div");
         middleDiv.setAttribute("class", "middle");
@@ -23,11 +27,13 @@ function drawImages(arrayOfImages) {
 
 
         middleDiv.append(textDiv);
-        
         div.append(imageProperty, middleDiv);
         document.body.appendChild(div)
+
     }
 
+
 }
-drawImages()
+drawImages();
+
 
